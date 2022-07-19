00aaba089bc7e4f92fd04bf1239b3250fec4f2c6
Roelof <r_j_vangolen@hotmail.com>
2022-07-19 16:20:35 +0200

    Fixed the use of the property enablePlaceholder

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -23,7 +23,7 @@ export class HomePage implements OnInit {
   defaultCountryIsoTest = "nl";
   dialCodePrefix = "+";
   enableAutoCountrySelect = false;
-  enablePlaceholder = true;
+  enablePlaceholder = false;
   fallbackPlaceholder = "";
   inputPlaceholder = "";
   maxLength = "15";
